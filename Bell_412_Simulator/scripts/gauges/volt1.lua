1d9eb07659f9f61a4b36e9954cd7f2ce8ba5545a
mbbsworld1045-lang <mbbsworld1045@gmail.com>
2026-04-06 17:02:24 +0500

    feat: implement dual voltmeter gauge scripts for AC/DC monitoring

--- Bell_412_Simulator/scripts/gauges/volt1.lua
+++ Bell_412_Simulator/scripts/gauges/volt1.lua
@@ -1,70 +1,107 @@
---B412EP Voltmeter #1--
---For use with X-Trident B412EP only--
---AC voltmeter runs off AC1--
---DC voltmeter runs off DC1--
+-- B412EP Voltmeter #1
+-- For use with X-Trident B412EP only
+-- AC voltmeter runs off AC1
+-- DC voltmeter runs off DC1
 
+-- ==========================================
+-- VALUE TO ANGLE CALIBRATION TABLES
+-- Format: { Simulator_Value, Needle_Angle }
+-- ==========================================
+local ac1_table = {
+    { 100, 20 },      -- 100V mark
+    { 111, 95 },      -- Observation point: 95 deg points to 111
+    { 115, 112 },     -- Calibrated angle to hit the 115 mark
+    { 120, 132 },     -- Estimated angle for 120
+    { 130, 170 }      -- 130V mark
+}
 
---Variables--
-local cur_ac1 = 100
-local tgt_ac1 = 100
-local cur_dc1 = 15
-local tgt_dc1 = 0
+local dc1_table = {
+    { 15,   -20 },      -- Min value
+    { 26.5, -136.67 },  -- Normal Operating Max
+    { 37,   -172.17 }   -- Hard Cap Max
+}
+
+-- Local Variables
+local cur_angle_ac1 = 20  
+local tgt_angle_ac1 = 20
+local cur_angle_dc1 = -20 
+local tgt_angle_dc1 = -20
 local factor = 0.09
 
-
---Add images--
+-- Add images
 -- img_add_fullscreen("VoltmeterBackplate.png")
 img_add("volt_meter_faceplate.png", 25, 25, 250, 250)
 -- img_add("volt_meter_1_bezel.png", 25, 25, 250, 250)
-img_needle_1 = img_add("Gauge_needle_1.png", 100.5, 80, 50, 140)
-img_needle_2 = img_add("Gauge_needle_2.png", 149.5, 80, 50, 140)
+
+-- Declared as 'local' to prevent cross-gauge conflicts
+local img_needle_1 = img_add("Gauge_needle_1.png", 100.5, 80, 50, 140)
+local img_needle_2 = img_add("Gauge_needle_2.png", 149.5, 80, 50, 140)
 img_add("Gauge_centre_blank.png", 25, 25, 250, 250)
 
-
---Get data for needle--
-function data(ac1, dc1)
-
-    --Set to run px on/off AC power--
-    if ac1==0 then
-        tgt_ac1 = 100
-    else
-        tgt_ac1 = ac1
+-- Interpolation Helper Function
+local function interpolate(tbl, value)
+    local last_v, last_a
+    for i, pair in ipairs(tbl) do
+        local v = pair[1]
+        local a = pair[2]
+        if value <= v then
+            if i == 1 then return a end
+            local pct = (value - last_v) / (v - last_v)
+            return last_a + (pct * (a - last_a))
+        end
+        last_v = v
+        last_a = a
     end
-    
-    --Set to run temp on/off DC power--
-    if dc1==0 then
-        tgt_dc1 = 15
-    else
-        tgt_dc1 = dc1
-    end
-    
+    return last_a 
 end
 
+-- Get data from Simulator
+function data(ac1, dc1, batt1_on)
+    -- Gauge only responds when Battery 1 switch (L:Swbatta) is ON
+    if not batt1_on or batt1_on == 0 then
+        tgt_angle_ac1 = interpolate(ac1_table, 100)
+        tgt_angle_dc1 = interpolate(dc1_table, 15)
+        return
+    end
 
---Animate needles--
+    local tgt_ac1 = ac1
+    local tgt_dc1 = dc1
+    
+    -- Set to drop to bottom if power is lost
+    if ac1 == 0 then tgt_ac1 = 100 end
+    if dc1 == 0 then tgt_dc1 = 15 end
+
+    -- Cap values just in case of simulator spikes
+    tgt_ac1 = var_cap(tgt_ac1, 100, 130)
+    tgt_dc1 = var_cap(tgt_dc1, 15, 30)
+
+    -- Look up the target angle from the tables
+    tgt_angle_ac1 = interpolate(ac1_table, tgt_ac1)
+    tgt_angle_dc1 = interpolate(dc1_table, tgt_dc1)
+end
+
+-- Animate needles
 function timer_callback()
-
-    --Rotate AC1 needle--
-    tgt_ac1 = math.max(100, math.min(130, tgt_ac1))
-    cur_ac1 = cur_ac1 + ((tgt_ac1 - cur_ac1) * factor)
-    rotate(img_needle_1, (150/30 * (cur_ac1 - 100)) + 20 )
     
-    --Rotate DC1 needle--
-    tgt_dc1 = math.max(15, math.min(30, tgt_dc1))
-    cur_dc1 = cur_dc1 + ((tgt_dc1 - cur_dc1) * factor)
-    rotate(img_needle_2, (116.667/11.5 * (-cur_dc1 + 15)) -20)
+    -- Smooth and rotate AC1 needle
+    cur_angle_ac1 = cur_angle_ac1 + ((tgt_angle_ac1 - cur_angle_ac1) * factor)
+    rotate(img_needle_1, cur_angle_ac1)
+            
+    -- Smooth and rotate DC1 needle
+    cur_angle_dc1 = cur_angle_dc1 + ((tgt_angle_dc1 - cur_angle_dc1) * factor)
+    rotate(img_needle_2, cur_angle_dc1)
     
 end
 
+-- Timer start
+local tmr_blink = timer_start(0, 50, timer_callback)
 
---Timer start--
-tmr_blink = timer_start(0, 50, timer_callback)
-
-
---DataRef Subscriptions--
+-- DataRef Subscriptions
 -- Using custom L-vars computed by NAV_GPS.xml (AC1 = 115V from inverter, DC1 = battery+gen voltage)
+-- Gauge only activates when Battery 1 (L:Swbatta) is ON
 fsx_variable_subscribe(
     "L:VoltAC1", "volt",                        
     "L:VoltDC1", "volt",
+    "L:Swbatta", "Bool",
     data
 )
--- Bell_412_Simulator/scripts/gauges/volt2.lua
+++ Bell_412_Simulator/scripts/gauges/volt2.lua
@@ -1,71 +1,112 @@
---B412EP Voltmeter #2--
---For use with X-Trident B412EP only--
---AC voltmeter runs off AC1--
---DC voltmeter runs off DC1--
+-- B412EP Voltmeter #2
+-- For use with X-Trident B412EP only
+-- AC voltmeter runs off AC2
+-- DC voltmeter runs off DC2
 
+-- ==========================================
+-- VALUE TO ANGLE CALIBRATION TABLES
+-- Format: { Simulator_Value, Needle_Angle }
+-- ==========================================
+local ac2_table = {
+    { 100, 20 },      -- 100V mark
+    { 111, 95 },      -- We know from your observation that 95 deg points to 111
+    { 115, 112 },     -- Adjusted angle to push the needle up to the actual 115 mark
+    { 120, 132 },     -- Estimated angle for 120
+    { 130, 170 }      -- 130V mark
+}
 
---Variables--
-local cur_ac2 = 100
-local tgt_ac2 = 100
-local cur_dc2 = 15
-local tgt_dc2 = 0
+local dc2_table = {
+    { 15,   -20 },      -- Min value
+    { 26.5, -136.67 },  -- Normal Operating Max
+    { 37,   -172.17 }   -- Hard Cap Max
+}
+
+-- Local Variables
+local cur_angle_ac2 = 20  
+local tgt_angle_ac2 = 20
+local cur_angle_dc2 = -20 
+local tgt_angle_dc2 = -20
 local factor = 0.09
 
-
---Add images--
+-- Add images
 -- img_add_fullscreen("VoltmeterBackplate.png")
 img_add("volt_meter_faceplate.png", 25, 25, 250, 250)
 -- img_add("volt_meter_2_bezel.png", 25, 25, 250, 250)
-img_needle_1 = img_add("Gauge_needle_1.png", 100.5, 80, 50, 140)
-img_needle_2 = img_add("Gauge_needle_2.png", 149.5, 80, 50, 140)
+
+-- Declared as 'local' to prevent cross-gauge conflicts
+local img_needle_1 = img_add("Gauge_needle_1.png", 100.5, 80, 50, 140)
+local img_needle_2 = img_add("Gauge_needle_2.png", 149.5, 80, 50, 140)
 img_add("Gauge_centre_blank.png", 25, 25, 250, 250)
 
-
---Get data for needle--
-function data(ac2, dc2)
-
-    --Set to run px on/off AC power--
-    if ac2==0 then
-    tgt_ac2 = 100
-    else
-    tgt_ac2 = ac2
+-- Interpolation Helper Function
+local function interpolate(tbl, value)
+    local last_v, last_a
+    for i, pair in ipairs(tbl) do
+        local v = pair[1]
+        local a = pair[2]
+        if value <= v then
+            if i == 1 then return a end
+            local pct = (value - last_v) / (v - last_v)
+            return last_a + (pct * (a - last_a))
+        end
+        last_v = v
+        last_a = a
     end
-    
-    --Set to run temp on/off DC power--
-    if dc2==0 then
-    tgt_dc2 = 15
-    else
-    tgt_dc2 = dc2
-    end
-
+    return last_a 
 end
 
+-- Get data from Simulator
+function data(ac2, dc2, batt2_on)
+    -- Gauge only responds when Battery 2 switch (L:Swbattb) is ON
+    if not batt2_on or batt2_on == 0 then
+        tgt_angle_ac2 = interpolate(ac2_table, 100)
+        tgt_angle_dc2 = interpolate(dc2_table, 15)
+        return
+    end
 
---Animate needles--
+    local tgt_ac2 = ac2
+    local tgt_dc2 = dc2
+    
+    -- Set to drop to bottom if power is lost
+    if ac2 == 0 then tgt_ac2 = 100 end
+    if dc2 == 0 then tgt_dc2 = 15 end
+
+    -- Cap values just in case of simulator spikes
+    tgt_ac2 = var_cap(tgt_ac2, 100, 130)
+    tgt_dc2 = var_cap(tgt_dc2, 15, 30)
+
+    -- Look up the target angle from the tables
+    tgt_angle_ac2 = interpolate(ac2_table, tgt_ac2)
+    tgt_angle_dc2 = interpolate(dc2_table, tgt_dc2)
+end
+
+-- Animate needles
 function timer_callback()
     
-    --Rotate AC2 needle--
-    tgt_ac2 = var_cap(tgt_ac2, 100, 130)
-    rotate(img_needle_1, (150/30 * (cur_ac2 - 100)) + 20 )
-    cur_ac2 = cur_ac2 + ((tgt_ac2 - cur_ac2) * factor)
+    -- Smooth and rotate AC2 needle
+    cur_angle_ac2 = cur_angle_ac2 + ((tgt_angle_ac2 - cur_angle_ac2) * factor)
+    rotate(img_needle_1, cur_angle_ac2)
             
-    --Rotate DC2 needle--
-    tgt_dc2 = var_cap(tgt_dc2, 15, 30)
-    rotate(img_needle_2, (116.667/11.5 * (-cur_dc2 + 15)) -20)
-    cur_dc2 = cur_dc2 + ((tgt_dc2 - cur_dc2) * factor)
+    -- Smooth and rotate DC2 needle
+    cur_angle_dc2 = cur_angle_dc2 + ((tgt_angle_dc2 - cur_angle_dc2) * factor)
+    rotate(img_needle_2, cur_angle_dc2)
     
 end
 
-    
---Timer start--
-tmr_blink = timer_start(0, 50, timer_callback)
+-- Timer start
+local tmr_blink = timer_start(0, 50, timer_callback)
 
-
---DataRef Subscriptions--
-
-xpl_dataref_subscribe("412/electrical/gauge_AC2_volts", "FLOAT",
-                      "412/electrical/gauge_DC2_volts", "FLOAT", data)
+-- DataRef Subscriptions
+xpl_dataref_subscribe(
+    "412/electrical/gauge_AC2_volts", "FLOAT",
+    "412/electrical/gauge_DC2_volts", "FLOAT", 
+    data
+)
                       
-fsx_variable_subscribe("L:VoltAC2", "volt",                        
-                       "L:VoltDC2", "volt",
-                       data)
+-- Gauge only activates when Battery 2 (L:Swbattb) is ON
+fsx_variable_subscribe(
+    "L:VoltAC2", "volt",                        
+    "L:VoltDC2", "volt",
+    "L:Swbattb", "Bool",
+    data
+)
